5c5e62561ca79e516319c0d184231ad912757b77
Dimas Vindyawan <dimasvindyawan.new@gmail.com>
2024-07-11 23:00:27 +0700

    fix: migration table

--- app/migration/migration.js
+++ app/migration/migration.js
@@ -6,6 +6,9 @@ const { insertUnits } = require("../src/models/units");
 const { insertUsers } = require("../src/models/users");
 const { insertPerusahaan } = require("../src/models/perusahaan");
 const { insertPeserta } = require("../src/models/peserta");
+const { insertPelatihanList } = require("../src/models/pelatihanList");
+const { insertLokasi } = require("../src/models/lokasi");
+const { insertInvoice } = require("../src/models/invoice")
 
 const migrateClassData = async () => {
   const { data } = dataFile;
@@ -19,17 +22,49 @@ const migrateClassData = async () => {
 
   let peserta = [];
 
+  let pelatihan_list = {
+    judul: "",
+    kategori: "",
+    sertifikasi: ""
+  }
+
+  let lokasi = {
+    tempat: "",
+    kota: ""
+  }
+
+  let invoice = {
+    nomor_invoice: "",
+    harga: "",
+    pajak: "",
+    konf_pajak: "",
+    terbayar: "",
+    status_pembayaran: "",
+    efaktur: "",
+    inv_pengiriman: "",
+    tgl_pengiriman_sertifikat: "",
+    penagihan_nama: "",
+    penagihan_kontak: "",
+    cashback: "",
+    divisi: "",
+    rekening_cashback: "",
+    tgl_cashback: "",
+    tgl_terbayar: "",
+    id_pelatihan: "",
+    id_perusahaan: "",
+  };
+
   for (let index = 0; index < data.length; index++) {
     const { data_perusahaan } = data[index];
 
+    // if perusahaan is empty then continue
     if (data_perusahaan === null || data_perusahaan === undefined) continue
-
-    // if (index === 20) break;
 
     const perus = JSON.parse(data_perusahaan);
     const migratePesertaPerusahaan = async (perus) => {
       let idPerusahaan = [];
       let idPeserta = [];
+      let idInvoice = []
 
       for (const perusahaanIteration of perus) {
         // set perusahaan
@@ -37,6 +72,28 @@ const migrateClassData = async () => {
         const resultPerusahaan = await insertPerusahaan(perusahaan);
 
         idPerusahaan.push(resultPerusahaan.insertId);
+
+        invoice = {
+          harga: parseInt(perusahaanIteration.harga),
+          pajak: perusahaanIteration.pajak,
+          konf_pajak: perusahaanIteration.konf_pajak,
+          status_pembayaran: perusahaanIteration.status_pembayaran,
+          nomor_invoice: perusahaanIteration.nomor_invoice,
+          efaktur: perusahaanIteration.efaktur,
+          cashback: perusahaanIteration.cashback,
+          inv_pengiriman: perusahaanIteration.pengiriman_invoice,
+          penagihan_kontak: perusahaanIteration.telp,
+          penagihan_nama: perusahaanIteration.cp_penagihan,
+          divisi: perusahaanIteration.divisi,
+          tgl_pengiriman_sertifikat: perusahaanIteration.tgl_pengiriman_sertifikat,
+          rekening_cashback: perusahaanIteration.rek_cashback,
+          tgl_cashback: perusahaanIteration.tgl_tf_cashback,
+          tgl_terbayar: perusahaanIteration.tgl_bayar,
+          id_perusahaan: resultPerusahaan.insertId,
+        }
+
+        const resultInvoice = await insertInvoice(invoice)
+        idInvoice.push(resultInvoice.insertId)
 
         // iterasi peserta
         let arrayOfPeserta = perusahaanIteration.peserta ?? []
@@ -82,14 +139,34 @@ const migrateClassData = async () => {
       return {
         idPerusahaan,
         idPeserta,
+        idInvoice
       };
 
     };
 
+    const migratePelatihanList = async () => {
+      pelatihan_list = {
+        judul: data[index].judul,
+        kategori: data[index].kategori,
+      }
+
+      return await insertPelatihanList(pelatihan_list);
+    }
+
+    const migrateLokasi = async () => {
+      lokasi = {
+        tempat: data[index].tempat_pelaksanaan,
+        kota: data[index].kota_pelaksanaan
+      }
+
+      return await insertLokasi(lokasi);
+    }
+
     // idPerusahaan and idPeserta
-    // let result = await migratePesertaPerusahaan(perus)
+    let result = await migratePesertaPerusahaan(perus)
 
     console.log(result)
+    if (index === 500) break
 
   }
 };
